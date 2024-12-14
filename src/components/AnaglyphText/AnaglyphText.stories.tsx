5e07491cd64c2585ef9e4a598b30384c88347d19
isoppp <prog@d2deck.com>
2024-12-14 11:22:39 +0900

    add AnaglyphText

---
+++ src/components/AnaglyphText/AnaglyphText.stories.tsx
@@ -0,0 +1,32 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import { AnaglyphText as Component } from './'
+
+const description = `
+### Additional Modifications
+
+- 
+`
+
+export default {
+  component: Component,
+  parameters: {
+    docs: {
+      description: {
+        component: description,
+      },
+    },
+  },
+} as Meta<typeof Component>
+
+export const Animate: StoryObj<typeof Component> = {
+  args: {
+    type: 'animate',
+  },
+}
+
+export const Static: StoryObj<typeof Component> = {
+  args: {
+    type: 'static',
+  },
+}
